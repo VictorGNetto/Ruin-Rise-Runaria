218dead90348bf2e1f44cda96097f0a1e9f546ee
Victor Netto <victornetto@discente.ufg.br>
2024-01-12 22:34:30 -0300

    Added rune details UI in rune selection

--- Ruin and Rise of Runaria/Assets/Scenes/Rune Selection and Execution Sandbox/Scripts/Selection/PickUpRuneUI.cs
+++ Ruin and Rise of Runaria/Assets/Scenes/Rune Selection and Execution Sandbox/Scripts/Selection/PickUpRuneUI.cs
@@ -6,11 +6,22 @@ using UnityEngine;
 public class PickUpRuneUI : MonoBehaviour
 {
     public ProgramUI programUI;
+    public GameObject details;
     public String runeName;
 
     public void Select()
     {
         GameObject duplicate = Instantiate(this.gameObject);
         programUI.InsertRune(duplicate, runeName);
+    }
+
+    public void ShowRuneDetails()
+    {
+        details.SetActive(true);
+    }
+
+    public void HideRuneDetails()
+    {
+        details.SetActive(false);
     }
 }
