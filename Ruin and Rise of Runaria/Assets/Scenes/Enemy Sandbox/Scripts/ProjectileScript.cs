66251b20a793d21ab6fd92da450c52c02771bf26
Rodrigo Barros Parreira <rodrigobp2003@gmail.com>
2024-01-25 00:22:19 -0300

    Wraith Enemy

--- Ruin and Rise of Runaria/Assets/Scenes/Enemy Sandbox/Scripts/ProjectileScript.cs
+++ Ruin and Rise of Runaria/Assets/Scenes/Enemy Sandbox/Scripts/ProjectileScript.cs
@@ -3,10 +3,27 @@ using UnityEngine;
 
 public class ProjectileScript : MonoBehaviour
 {
+<<<<<<< Updated upstream
+=======
+    private bool hit;
+    private BoxCollider2D boxCollider;
+    private Animator anim;
+>>>>>>> Stashed changes
     public float speed = 5f;
     public Golem target;
     public float damage;
 
+<<<<<<< Updated upstream
+=======
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+
+>>>>>>> Stashed changes
     // Update is called once per frame
     void Update()
     {
@@ -18,7 +35,18 @@ public class ProjectileScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
+<<<<<<< Updated upstream
         target.health = Math.Max(0, target.health - damage);
         Destroy(gameObject);
     }
 }
+=======
+        boxCollider.enabled = false;
+        anim.SetTrigger("Explode");
+
+
+        target.health = Math.Max(0, target.health - damage);
+        Destroy(gameObject, 0.2f);
+    }
+}
+>>>>>>> Stashed changes
