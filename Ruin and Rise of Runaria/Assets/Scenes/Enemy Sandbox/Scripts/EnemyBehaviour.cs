f1c077a205eea8d2e001eff175a5edbd09758cd6
Rodrigo Barros Parreira <rodrigobp2003@gmail.com>
2024-01-27 19:52:01 -0300

    Wraith and Minotaur improvements
    Upgraded the algorithms of both enemies

--- Ruin and Rise of Runaria/Assets/Scenes/Enemy Sandbox/Scripts/EnemyBehaviour.cs
+++ Ruin and Rise of Runaria/Assets/Scenes/Enemy Sandbox/Scripts/EnemyBehaviour.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+using UnityEngine.UIElements;
 
-public class EnemyBehaviour : MonoBehaviour
+public class EnemyBehaviour : MonoBehaviour, ICharacter
 {
     // Start is called before the first frame update
     public Transform launchOffset;
@@ -12,7 +14,20 @@ public class EnemyBehaviour : MonoBehaviour
     public Animator anim;
     public Vector3 scale;
     public SpriteRenderer spriteRenderer;
-    
+    public Golem target = null;
+    public float attackCooldown;
+    private float nextAttackTime = 0.2f;
+    private bool thereAreGolems = true;
+
+
+    public float defense;
+    public float health = 75;
+    public float maxHealth = 100;
+    public EnemyHealthBar enemyHealthBar;
+
+    public bool alive = true;
+
+    private int guid;
 
 
     private void Awake()
@@ -26,6 +41,7 @@ public class EnemyBehaviour : MonoBehaviour
 
     Golem ClosestGolem()
     {
+        
         Golem closestGolem = null;
         float leastDistance;
         leastDistance = Mathf.Infinity;
@@ -37,11 +53,45 @@ public class EnemyBehaviour : MonoBehaviour
                 closestGolem = levelDirector.golems[i];
             }
         }
-        
+        if (closestGolem == null)
+        {
+            thereAreGolems = false;
+        }
         return closestGolem;
     }
 
-    IEnumerator Fire()
+    private void Update()
+    {
+
+        if (!alive) return;
+
+        enemyHealthBar.SetHealth(health, maxHealth);
+
+        if (health <= 0)
+        {
+            alive = false;
+            enemyHealthBar.gameObject.SetActive(false);
+            Die();
+        }
+
+        if(thereAreGolems) { 
+            
+            if (target == null || !target.alive)
+            {
+                target = ClosestGolem();
+            }
+
+            if (Time.time >= nextAttackTime)
+            {
+                Fire();
+                nextAttackTime = Time.time + attackCooldown;
+            }
+        }
+
+
+    }
+
+    public void Fire()
     {
 
         if(ClosestGolem().transform.position.x < transform.position.x)
@@ -53,28 +103,81 @@ public class EnemyBehaviour : MonoBehaviour
             spriteRenderer.flipX = false;
         }
 
-
-        anim.SetTrigger("Cast");
-
-        yield return new WaitForSeconds(0.5f);
-
-        Vector3 direction = ClosestGolem().transform.position - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        GameObject proj = Instantiate(projectile, launchOffset.position, Quaternion.Euler(new Vector3(0, 0, angle)));
-
+        if (target.alive && target != null)
+        {
+            anim.SetTrigger("Cast");
+            Vector3 direction = ClosestGolem().transform.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            GameObject proj = Instantiate(projectile, launchOffset.position, Quaternion.Euler(new Vector3(0, 0, angle)));
+            proj.GetComponent<ProjectileScript>().target = ClosestGolem();
+            
+        }
+        else
+        {
+            target = ClosestGolem();
+        }
         
-        //GameObject proj = Instantiate(projectile, launchOffset.position, Quaternion.identity);
-
-        proj.GetComponent<ProjectileScript>().target = ClosestGolem();
-        yield return new WaitForSeconds(1.5f);
-        StartCoroutine(Fire());
     }
 
-    private void Start()
+    
+
+
+
+    public ICharacter Target()
     {
-        StartCoroutine(Fire());
-        
-        
+        return target;
     }
+
+    public int GetSortingOrder()
+    {
+        return spriteRenderer.sortingOrder;
+    }
+
+    public Vector3 TargetPosition()
+    {
+        return target.transform.position;
+    }
+
+    public Vector3 Position()
+    {
+        return transform.position;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        if (!alive) return;
+
+        amount = (1 - defense) * amount;
+        health = Mathf.Max(0, health - amount);
+        enemyHealthBar.SetHealth(health, maxHealth);
+        if (health == 0) Die();
+    }
+
+    public void Heal(float amount)
+    {
+        if (!alive) return;
+
+        health = Mathf.Min(maxHealth, health + amount);
+        enemyHealthBar.SetHealth(health, maxHealth);
+    }
+
+    public float MaxHealth()
+    {
+        return maxHealth;
+    }
+
+    public void Die()
+    {
+        alive = false;
+        anim.SetTrigger("Die");
+        Destroy(gameObject, 2.0f);
+    }
+
+    public int GUID()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
 
 }
--- Ruin and Rise of Runaria/Assets/Scenes/Enemy Sandbox/Scripts/MinotaurBehaviour.cs
+++ Ruin and Rise of Runaria/Assets/Scenes/Enemy Sandbox/Scripts/MinotaurBehaviour.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 using static UnityEngine.GraphicsBuffer;
 
-public class MinotaurBehaviour: MonoBehaviour
+public class MinotaurBehaviour: MonoBehaviour, ICharacter
 {
-    // Start is called before the first frame update
+    
     public Transform launchOffset;
     public LevelDirector levelDirector;
     public GameObject projectile = null;
@@ -17,6 +18,17 @@ public class MinotaurBehaviour: MonoBehaviour
     public float range;
     public float damage;
     public float attackCooldown;
+    public Golem target = null;
+    private bool thereAreGolems = true;
+
+    public float defense;
+    public float health = 75;
+    public float maxHealth = 100;
+    public EnemyHealthBar enemyHealthBar;
+
+    public bool alive = true;
+
+    private int guid;
 
     private void Awake()
     {
@@ -31,26 +43,32 @@ public class MinotaurBehaviour: MonoBehaviour
 
     Golem LowestGolem()
     {
+        
         Golem lowestGolem = null;
         float leastHealth;
         leastHealth= Mathf.Infinity;
         for (int i = 0; i < levelDirector.golems.Count; i++)
         {
-            if (levelDirector.golems[i] != null && levelDirector.golems[i].health < leastHealth)
+            if (levelDirector.golems[i] != null && levelDirector.golems[i].health < leastHealth && levelDirector.golems[i].alive)
             {
                 leastHealth = levelDirector.golems[i].health;
                 lowestGolem = levelDirector.golems[i];
             }
         }
 
+        if (lowestGolem == null)
+        {
+            thereAreGolems = false;
+        }
         return lowestGolem;
+
     }
 
     public void Walk()
     {
         anim.SetBool("Walking", true);
-        agent.SetDestination(LowestGolem().transform.position);
-        if (LowestGolem().transform.position.x < transform.position.x)
+        agent.SetDestination(target.Position());
+        if (target.Position().x < Position().x)
         {
             spriteRenderer.flipX = true;
         }
@@ -62,36 +80,120 @@ public class MinotaurBehaviour: MonoBehaviour
     private float nextAttackTime;
     private void Update()
     {
-        if (Vector2.Distance(transform.position, LowestGolem().transform.position) >= range)
-        {
-            Walk();
-        }
-        else
-        {
-            anim.SetBool("Walking", false);
-        }
 
-        if (Vector2.Distance(transform.position, LowestGolem().transform.position) <= range && Time.time >= nextAttackTime)
+        if (!alive) return;
+
+        enemyHealthBar.SetHealth(health, maxHealth);
+
+        if (health <= 0)
         {
-            Attack();
-            nextAttackTime = Time.time + attackCooldown;
+            alive = false;
+            enemyHealthBar.gameObject.SetActive(false);
+            Die();
         }
 
 
+
+
+        if (thereAreGolems)
+        {
+
+            if (target == null || !target.alive)
+            {
+                target = LowestGolem();
+            }
+
+            if (Vector2.Distance(transform.position, target.transform.position) >= range)
+            {
+                Walk();
+            }
+            else
+            {
+                anim.SetBool("Walking", false);
+            }
+
+            if (Vector2.Distance(transform.position, target.transform.position) <= range && Time.time >= nextAttackTime)
+            {
+                Attack();
+                nextAttackTime = Time.time + attackCooldown;
+            }
+        }
+
+        
+        
+
+
+
+    }
+
+    public void DealDamage()
+    {
+        target.TakeDamage(damage);
     }
 
     public void Attack()
     {
 
-        
-
-
         anim.SetTrigger("Attack");
-        LowestGolem().health = Mathf.Max(0, LowestGolem().health - damage);
-        
-        
+
     }
 
-    
 
+
+
+
+    public ICharacter Target()
+    {
+        return target;
+    }
+
+    public int GetSortingOrder()
+    {
+        return spriteRenderer.sortingOrder;
+    }
+
+    public Vector3 TargetPosition()
+    {
+        return target.transform.position;
+    }
+
+    public Vector3 Position()
+    {
+        return transform.position;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        if (!alive) return;
+
+        amount = (1 - defense) * amount;
+        health = Mathf.Max(0, health - amount);
+        enemyHealthBar.SetHealth(health, maxHealth);
+        if (health == 0) Die();
+    }
+
+    public void Heal(float amount)
+    {
+        if (!alive) return;
+
+        health = Mathf.Min(maxHealth, health + amount);
+        enemyHealthBar.SetHealth(health, maxHealth);
+    }
+
+    public float MaxHealth()
+    {
+        return maxHealth;
+    }
+
+    public void Die()
+    {
+        alive = false;
+        anim.SetTrigger("Die");
+        Destroy(gameObject, 2.0f);
+    }
+
+    public int GUID()
+    {
+        throw new System.NotImplementedException();
+    }
 }
